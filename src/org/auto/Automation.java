fe6a231ca97bfba7f995b6fbf9f4612ccf7ca0fd
MayilsamyMs <poojamayilsamy.3695@gmail.com>
2021-08-10 18:54:41 +0530

    my first push

---
+++ src/org/auto/Automation.java
@@ -0,0 +1,34 @@
+package org.auto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation {
+	
+public static void main(String[] args) {
+	
+	
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\eclipse-workspace\\Automation\\Driver\\chromedriver.exe");	
+	
+	
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get(" http://gmail.com/");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
